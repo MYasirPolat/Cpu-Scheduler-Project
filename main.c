d2913f663b7cb575c21f429512bd7caf91856ac5
MYasirPolat <91343247+MYasirPolat@users.noreply.github.com>
2024-05-23 17:12:46 +0300

    Update 4
    Function for round robin algorithm is added.

--- main.c
+++ main.c
@@ -77,7 +77,7 @@ void assignCpus(FILE* file, struct Process processes[], int processCount) {
     }
 }
 
-//Functions to process the processes with priority 0 at cpu1
+//Functions to process the processes with priority 0 at cpu2 with First Come First Serve Algorithm
 void FCFS(FILE* file,struct Process processes[],int processCount,int priority0Count) {
     struct Process priority0[priority0Count];   //Struct to hold every process with priority 0
     //Add all priority 0 processes to cpu1 queue
@@ -110,7 +110,7 @@ void FCFS(FILE* file,struct Process processes[],int processCount,int priority0Co
     }
 }
 
-//Functions to process the processes with priority 1 at cpu2
+//Functions to process the processes with priority 1 at cpu2 with Shortes Job First Algorithm
 void SJF(FILE* file, struct Process processes[], int processCount, int priority1Count) {
     int elapsedTime = 0;        //Integer for storing the time that has passed
     int processedCount = 0;     //Integer for storing the processes that are terminated
@@ -147,6 +147,71 @@ void SJF(FILE* file, struct Process processes[], int processCount, int priority1
     }
 }
 
+//Function process the processes with priority 2 and 3 at cpu2 with Round Robin Algorithm
+void roundRobin(FILE* file, struct Process processes[], int processCount, int priorityCount, int quantumTime) {
+    int priority = (quantumTime == 8) ? 2 :  3;     //Decides the priority depending on the given quantum time
+    int processedCount = 0;
+    int elapsedTime = 0;
+    struct Process queue[100];    //Holds the processes in the queue
+    struct Process delay;         //Holds the process that isnt finished in a given quantun time
+    int front = 0;  //Holds begining index of the queue
+    int rear = 0;  //Holds ending index of the queue
+
+    printf("\nCPU-2 que%d(priority-%d) (RR%d) ->",priority+1,priority,quantumTime);
+    // Initialize the queue with priority 2 processes that have arrived by time 0
+    for (int i = 0; i < processCount; i++) {
+        if (processes[i].priority == priority && processes[i].arrivalTime <= elapsedTime) {
+            queue[rear++] = processes[i];
+        }
+    }
+
+    // Main loop to process all processes
+    while (processedCount < priorityCount) {
+
+        if (front != rear) {  // Check if queue is not empty
+            struct Process *currentProcess = &queue[front]; //Initialized to point the current process in the cpu2
+
+            if (processes[currentProcess->processNumber - 1].isTerminated ==false && isEnoughResource(2,currentProcess->ram,currentProcess->cpuUsage)) {  // Checks resources and if process has not been terminated
+                int runTime = currentProcess->burstTime < quantumTime ? currentProcess->burstTime : quantumTime;    //Decides how much a process will run at each iteration
+                fprintf(file, "Process %d is assigned to CPU-2.\n", currentProcess->processNumber);
+                printf("P%d,",currentProcess->processNumber);
+                currentProcess->burstTime -= runTime;   //Decreases process' burst time by how much it ran
+                elapsedTime += runTime;                 //Keeps track of the time that has elapsed
+
+                // Assigns the current process to delay so it can be added at the end of the queue
+                if (currentProcess->burstTime > 0) {
+                    delay= *currentProcess;
+                    delay.isQueued=true;
+                }
+                //Terminates the process if it completed
+                else {
+                    fprintf(file, "Process %d is completed and terminated.\n", currentProcess->processNumber);
+                    processes[currentProcess->processNumber - 1].isTerminated = true;
+                    processedCount++;
+                }
+            }
+            front++;  //Move front index to next process
+        }
+        //Cpu is in idle increase elapsed time by 1
+        else {
+            elapsedTime++;
+        }
+
+        // Check if new processes have arrived to be added to the queue
+        for (int i = 0; i < processCount ; i++) {
+            if (processes[i].priority == priority && processes[i].arrivalTime <= elapsedTime && processes[i].isTerminated==false && processes[i].isQueued==false ) {
+                queue[rear++] = processes[i];
+                processes[i].isQueued =true;
+            }
+        }
+        //Adds the unfinished process to the end of the queue
+        if(delay.isQueued==true) {
+            queue[rear++] = delay;
+            delay.isQueued=false;
+            fprintf(file, "Process %d run until the defined quantum time and is queued again because the process is not completed.\n", delay.processNumber);
+        };
+    }
+}
 
 int main(void)
 {
@@ -171,7 +236,9 @@ int main(void)
     assignCpus(outputFile,processes,processCount);                                      //Assigns processes to cpus
     FCFS(outputFile,processes,processCount,count0);                         //Runs the FCFS algortihm for priority 0 processes in cpu1
     SJF(outputFile,processes,processCount,count1);                          //Runs the SJF algortihm for priority 1 processes in cpu2
-  
+    roundRobin(outputFile, processes, processCount,count2, 8);    //Runs the RR algortihm for priority 2 processes in cpu2
+    roundRobin(outputFile, processes, processCount,count3, 16);   //Runs the RR algortihm for priority 3 processes in cpu2
+
     //Closes the files
     fclose(inputFile);
     fclose(outputFile);
